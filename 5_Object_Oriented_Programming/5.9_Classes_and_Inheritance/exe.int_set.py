a9729df1dc840a932b17752d67953a43724be009
luccazade <la.wetter.sanchez@gmail.com>
2020-07-21 19:56:18 +0100

    Classes and Inheritance FX

---
+++ 5_Object_Oriented_Programming/5.9_Classes_and_Inheritance/exe.int_set.py
@@ -0,0 +1,56 @@
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self"""
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+           Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+           Raises ValueError if e is not in self"""
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def __str__(self):
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+
+    def intersect(self, other):
+        together = intSet()
+        for i in self.vals:
+            if i in other.vals:
+                together.insert(i)
+        return together
+
+    def __len__(self):
+        return len(self.vals)
+
+
+xset = intSet()
+yset = intSet()
+
+xset.insert(2)
+xset.insert(3)
+yset.insert(1)
+yset.insert(2)
+print(xset)
+print(yset)
+s = xset.intersect(yset)
+
+print(s)
+print(len(s))
